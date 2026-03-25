d8929c0fc8942f54184450085399a4a51c2cfa73
rmagesto <rmagesto@gmail.com>
2026-03-25 11:40:13 -0300

    Adicionando dados faltantes venda.py

--- venda.py
+++ venda.py
@@ -11,3 +11,13 @@ class Venda:
         self.produto = produto
         self.quantidade = quantidade
         self.pagamento = pagamento
+
+    def exibir_detalhes(self):
+        print("\n--- DETALHE DA VENDA ---")
+        print(f"Produto: {self.produto.nome}")
+        print(f"Quantidade: {self.quantidade}")
+        print(f"Consumido por: {self.pagamento.nome}")
+        print(f"Categoria: {self.pagamento.categoria}")
+        print(f"Curso: {self.pagamento.curso}")
+        print(f"Valor pago: R$ {self.pagamento.valor:.2f}")
+        print(f"Data/Hora: {self.pagamento.data_hora}")
